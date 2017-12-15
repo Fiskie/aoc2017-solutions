b3c358344a169d9e95afacc3d86a49ae104fb7db
fisk <myself@fiskie.me>
2017-12-15 09:03:33 +0000

    Use a buffer to reduce context switching

--- day15/day15.go
+++ day15/day15.go
@@ -12,20 +12,20 @@ func generator(c chan int, factor int, value int, multiple int) {
 }
 
 func main() {
-	fmt.Printf("Part 1: %d\n", part1(40000000,0, 0))
-	fmt.Printf("Part 2: %d\n", part1(5000000,4, 8))
+	fmt.Printf("Part 1: %d\n", iterate(40000000,0, 0))
+	fmt.Printf("Part 2: %d\n", iterate(5000000,4, 8))
 }
 
-func part1(iterations int, aMult int, bMult int) int {
-	a := make(chan int)
-	b := make(chan int)
+func iterate(iterations int, aMult int, bMult int) int {
+	a := make(chan int, 50)
+	b := make(chan int, 50)
 	count := 0
 
 	go generator(a,16807, 873, aMult)
 	go generator(b,48271, 583, bMult)
 
 	for i := 0; i < iterations; i++ {
-		if <-a &^ 0xFFFF0000 == <-b &^ 0xFFFF0000 {
+		if <-a & 0xFFFF == <-b & 0xFFFF {
 			count += 1
 		}
 	}
